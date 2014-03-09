30f46ca13f554c9b6fe582694728093c1b5f8a5a
Ang Gao <ang.gao87@gmail.com>
2014-03-09 00:34:58 +0000

    add post template

--- generator.py
+++ generator.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'hello world'
 
+@app.route('/blog/post/')
+def post():
+    return render_template('post.html', post_content = 'Hello world !')
 if __name__ == '__main__':
     app.run(port=8000)
